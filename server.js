677a444c6135ee0ec80697c578ec1a56c06c8135
caldric <cvaldric@gmail.com>
2020-08-07 12:52:56 -0400

    Create basic Express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+}
+
+app.get('/', (_, res) => {
+  res.send('Hello world!');
+});
+
+// app.get('/*', (_, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});

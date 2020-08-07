f240851a54a47dcc8f1dd822a9ec07e01f2b8831
caldric <cvaldric@gmail.com>
2020-08-07 13:49:59 -0400

    Add photo of me in About Me section

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h2>About Me</h2>
+      <img src="./me.png" alt="My picture" />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur,
         neque qui fuga, doloremque corrupti modi laborum expedita porro cumque

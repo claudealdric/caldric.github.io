b008c8d416e3ec37e351e5b617f3dd36864aa4fe
caldric <cvaldric@gmail.com>
2020-08-07 13:40:59 -0400

    Create nav component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Index from './components/Index';
 import Portfolio from './components/Portfolio';
+import Nav from './components/Nav';
 
 const App = () => {
   return (
     <div>
+      <Nav />
       <Switch>
         <Route exact path="/">
           <Index />
---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>About Me</li>
+        <li>Resume</li>
+        <li>Toolset</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;

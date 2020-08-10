fdc685fc675ba5fb720e36cbd9b3de162fe59f20
caldric <cvaldric@gmail.com>
2020-08-10 13:33:12 -0400

    Convert button to Bootstrap button

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const Header: React.FC = () => {
   return (
     <div className="top-content">
       <h1>Claude Vergil Aldric</h1>
       <p>Software Engineer</p>
-      <button type="button">Download Resume</button>
+      <Button variant="primary">Download Resume</Button>
     </div>
   );
 };

32a097c41dcf3bdc9c70e9a096cd46f3f25a33a5
caldric <cvaldric@gmail.com>
2020-08-10 13:25:39 -0400

    Add skill icons

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.css';
 
 const Home: React.FC = () => {
   return (
@@ -14,28 +15,54 @@ const Home: React.FC = () => {
 
       <h2>Skills</h2>
       <ul>
-        <li>TypeScript</li>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Python</li>
-        <li>MongoDB</li>
-        <li>MySQL</li>
-        <li>PostgreSQL</li>
-        <li>MATLAB</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>React.js</li>
-        <li>jQuery</li>
-        <li>Bootstrap</li>
-        <li>Flask</li>
-        <li>NumPy</li>
-        <li>SciPy</li>
-        <li>Pandas</li>
-        <li>Matplotlib</li>
-        <li>Git</li>
-        <li>GitHub</li>
-        <li>Vim</li>
+        <li>
+          <img className="skills" src="./typescript.svg" alt="TypeScript" />
+        </li>
+        <li>
+          <img className="skills" src="./javascript.svg" alt="JavaScript" />
+        </li>
+        <li>
+          <img className="skills" src="./html5.svg" alt="HTML5" />
+        </li>
+        <li>
+          <img className="skills" src="./css.svg" alt="CSS3" />
+        </li>
+        <li>
+          <img className="skills" src="./python.svg" alt="Python" />
+        </li>
+        <li>
+          <img className="skills" src="./mongodb.svg" alt="MongoDB" />
+        </li>
+        <li>
+          <img className="skills" src="./mysql.svg" alt="MySQL" />
+        </li>
+        <li>
+          <img className="skills" src="./postgresql.svg" alt="PostgreSQL" />
+        </li>
+        <li>
+          <img className="skills" src="./nodejs.svg" alt="Node.js" />
+        </li>
+        <li>
+          <img className="skills" src="./express.svg" alt="Express.js" />
+        </li>
+        <li>
+          <img className="skills" src="./react.svg" alt="React.js" />
+        </li>
+        <li>
+          <img className="skills" src="./jquery.svg" alt="jQuery" />
+        </li>
+        <li>
+          <img className="skills" src="./bootstrap.svg" alt="Bootstrap" />
+        </li>
+        <li>
+          <img className="skills" src="./git.svg" alt="Git" />
+        </li>
+        <li>
+          <img className="skills" src="./github.svg" alt="GitHub" />
+        </li>
+        <li>
+          <img className="skills" src="./vim.svg" alt="Vim" />
+        </li>
       </ul>
     </div>
   );

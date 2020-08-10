ea7f79d2b1b8c1d6c7ba7f184035ba6221de6b2e
caldric <cvaldric@gmail.com>
2020-08-10 11:20:29 -0400

    Add brand statement

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -6,10 +6,10 @@ const Home: React.FC = () => {
       <h2>About Me</h2>
       <img src="./me.png" alt="My picture" />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur,
-        neque qui fuga, doloremque corrupti modi laborum expedita porro cumque
-        odio asperiores deserunt nemo! Reprehenderit deleniti similique
-        voluptates odit corporis quasi!
+        I am a lifelong learner who rapidly assimilates complex concepts and
+        strives for absolute excellence in my craft. I automatically deconstruct
+        and rebuild every idea or system I encounter while asking questions
+        like, “What is important,” or “Can this be improved?”
       </p>
 
       <h2>Skills</h2>

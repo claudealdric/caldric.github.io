37867560661104de48f8fe9e75ae79d84a4ed32c
caldric <cvaldric@gmail.com>
2020-08-10 11:01:07 -0400

    Add Bootstrap and React Bootstrap to dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
+// React & Bootstrap
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Components
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Nav from './components/Nav';

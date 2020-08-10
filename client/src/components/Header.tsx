e978db1ad2311345036d71e23a0ed7195dbfdc01
caldric <cvaldric@gmail.com>
2020-08-10 13:00:14 -0400

    Make navbar sticky

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <div>
+    <div className="top-content">
       <h1>Claude Vergil Aldric</h1>
       <p>Software Engineer</p>
       <button type="button">Download Resume</button>
--- client/src/components/NavComponent.tsx
+++ client/src/components/NavComponent.tsx
@@ -2,29 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './NavComponent.css';
 
 const NavComponent: React.FC = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand as={Link} to="/">
-        Claude Aldric
-      </Navbar.Brand>
-      <Nav className="ml-auto">
-        <Nav.Link as={Link} to="#">
-          About Me
-        </Nav.Link>
-        <Nav.Link as={Link} to="#">
-          Resume
-        </Nav.Link>
-        <Nav.Link as={Link} to="#">
-          Toolset
-        </Nav.Link>
-        <Nav.Link as={Link} to="/portfolio">
-          Portfolio
-        </Nav.Link>
-        <Nav.Link as={Link} to="#">
-          Contact
-        </Nav.Link>
+    <Navbar bg="dark" variant="dark" className="sticky">
+      <Nav className="container-fluid">
+        <Navbar.Brand as={Link} to="/">
+          Claude Aldric
+        </Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="#">
+            About Me
+          </Nav.Link>
+          <Nav.Link as={Link} to="#">
+            Resume
+          </Nav.Link>
+          <Nav.Link as={Link} to="#">
+            Toolset
+          </Nav.Link>
+          <Nav.Link as={Link} to="/portfolio">
+            Portfolio
+          </Nav.Link>
+          <Nav.Link as={Link} to="#">
+            Contact
+          </Nav.Link>
+        </Nav>
       </Nav>
     </Navbar>
   );

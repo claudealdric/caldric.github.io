a897856e341066345cb1422acb897660696c6897
caldric <cvaldric@gmail.com>
2020-08-11 04:44:53 -0400

    Revise footer styling

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="container">
+    <footer className="container-fluid">
       <hr />
       <p className="float-right">
         <a

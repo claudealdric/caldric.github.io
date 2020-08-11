3aa88321711018506a9494dd26bc50b25b61e7f6
caldric <cvaldric@gmail.com>
2020-08-11 04:48:12 -0400

    Center h1 name on small screens

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>Claude V. Aldric</h1>
+      <h1 className="text-center">Claude V. Aldric</h1>
       <p className="title">Software Engineer</p>
       <Button variant="primary">Download Resume</Button>
     </header>

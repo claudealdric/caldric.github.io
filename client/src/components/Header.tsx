fc467c9ee52d65243b140f82ef102218c34432a9
caldric <cvaldric@gmail.com>
2020-08-11 00:53:14 -0400

    Add background image to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import './Header.css';
 
 const Header: React.FC = () => {
   return (
-    <section className="jumbotron text-center">
-      <h1>Claude Vergil Aldric</h1>
+    <section className="jumbotron text-center header">
+      <div className="background-image"></div>
+      <h1>Claude V. Aldric</h1>
       <p>Software Engineer</p>
       <Button variant="primary">Download Resume</Button>
     </section>

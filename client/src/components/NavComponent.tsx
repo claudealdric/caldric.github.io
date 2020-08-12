2d9e578cfeb9bfa41cac849555fe0dcc1189ab02
caldric <cvaldric@gmail.com>
2020-08-12 03:33:59 -0400

    Add animation to nav bar using plain CSS

--- client/src/components/NavComponent.tsx
+++ client/src/components/NavComponent.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
+import './NavComponent.css';
+
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 const NavComponent: React.FC = () => {
   return (
-    <Navbar bg="dark" variant="dark" className="fixed-top">
+    <Navbar bg="dark" variant="dark" className="fixed-top" id="navbar">
       <Nav className="container-fluid">
         <Navbar.Brand as={Link} to="#top">
           Claude Aldric

8753bedba4a56f3c205c5165c616fbd064e37315
caldric <cvaldric@gmail.com>
2020-08-12 03:28:24 -0400

    Add animation to tools section

--- client/src/components/Skills.tsx
+++ client/src/components/Skills.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Skills.css';
+//@ts-ignore
+import Fade from 'react-reveal/Fade';
 
 import { skills } from '../skills';
 
@@ -7,15 +9,26 @@ const Skills = () => {
   return (
     <div id="tools" className="album">
       <div className="container">
-        <h2 className="text-center mb-5">Tools</h2>
-        <div className="row">
-          {skills.map((skill) => (
-            <div className="col-lg-2 col-md-3 col-sm-4 skill" key={skill.name}>
-              <img className="skill-img" src={skill.imgSrc} alt={skill.name} />
-              <p className="text-muted">{skill.name}</p>
-            </div>
-          ))}
-        </div>
+        <Fade bottom>
+          <h2 className="text-center mb-5">Tools</h2>
+        </Fade>
+        <Fade bottom cascade distance={'50%'}>
+          <div className="row">
+            {skills.map((skill) => (
+              <div
+                className="col-lg-2 col-md-3 col-sm-4 skill"
+                key={skill.name}
+              >
+                <img
+                  className="skill-img"
+                  src={skill.imgSrc}
+                  alt={skill.name}
+                />
+                <p className="text-muted">{skill.name}</p>
+              </div>
+            ))}
+          </div>
+        </Fade>
       </div>
     </div>
   );

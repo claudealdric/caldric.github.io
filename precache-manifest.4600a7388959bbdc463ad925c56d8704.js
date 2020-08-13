5c71caa6b7104206c87e7a086cea8ce397527337
caldric <cvaldric@gmail.com>
2020-08-13 00:59:51 -0400

    Updates

---
+++ precache-manifest.4600a7388959bbdc463ad925c56d8704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c9167fc12cd8278299dcd2e8f856a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e11e8e6163837fa6dd4",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7375f6d785cdf3fa4bc0",
+    "url": "/static/css/main.fdf06578.chunk.css"
+  },
+  {
+    "revision": "4e11e8e6163837fa6dd4",
+    "url": "/static/js/2.039bacda.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.039bacda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7375f6d785cdf3fa4bc0",
+    "url": "/static/js/main.792de41f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);

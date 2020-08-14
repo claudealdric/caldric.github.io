48144fe2a77575df3e5d04ec8872171cc4dac056
caldric <cvaldric@gmail.com>
2020-08-13 21:43:41 -0400

    Convert about me section into responsive grid

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,19 +7,29 @@ const AboutMe: React.FC = () => {
   return (
     <section className="jumbotron text-center" id="about-me">
       <div className="container">
-        <Fade bottom distance={'20%'}>
-          <img src="./me.png" alt="Claude Aldric" className="personal-photo" />
-        </Fade>
-        <Fade bottom>
-          <h2>About Me</h2>
-          <p className="lead text-muted">
-            I am a lifelong learner who rapidly assimilates complex concepts and
-            strives for absolute excellence in my craft. I automatically
-            deconstruct and rebuild every idea or system I encounter while
-            asking questions like, “What is important,” or “Can this be
-            improved?”
-          </p>
-        </Fade>
+        <div className="row">
+          <div className="col-md-4 photo-section">
+            <Fade bottom distance={'20%'}>
+              <img
+                src="./me.png"
+                alt="Claude Aldric"
+                className="personal-photo"
+              />
+            </Fade>
+          </div>
+          <div className="col-md-8 brand-statement">
+            <Fade bottom>
+              <h2>About Me</h2>
+              <p className="lead text-muted">
+                I am a lifelong learner who rapidly assimilates complex concepts
+                and strives for absolute excellence in my craft. I automatically
+                deconstruct and rebuild every idea or system I encounter while
+                asking questions like, “What is important,” or “Can this be
+                improved?”
+              </p>
+            </Fade>
+          </div>
+        </div>
       </div>
     </section>
   );

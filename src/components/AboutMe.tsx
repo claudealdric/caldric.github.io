44d00b695cfccd21fce15f69bf49548d361b84be
caldric <cvaldric@gmail.com>
2021-03-20 19:02:02 -0400

    Reword 'About Me' section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -12,26 +12,25 @@ const AboutMe: React.FC = () => {
             <Fade bottom>
               <h2>About Me</h2>
               <p className="lead text-muted">
-                Hello! I am a full-stack software developer who loves building
-                apps through Jira and the Agile Scrum framework. I discovered my
-                passion for software development late into my college career, so
-                I continued with my original major and did a coding bootcamp in
-                May 2020 to help transition into the tech industry. I view
-                technology as an amplifier of human capabilities and as the key
-                to the future, so I am particularly passionate about using it to
-                make a positive impact on others.
+                Hello! I am a full-stack software engineer who loves building
+                apps with the Agile approach. I discovered my passion for
+                software development late into my college career, so I continued
+                with my original major and did a coding bootcamp in May 2020 to
+                help transition into the tech industry. I view technology as an
+                amplifier of human capabilities and as the key to the future, so
+                I am particularly passionate about using it to make a positive
+                impact on others.
               </p>
               <p className="lead text-muted">
-                I have over 5 years of experience creating software solutions as
-                an analyst in the investment industry, where I spearheaded 8
-                data analytics and automation projects that saves over 650 hours
-                per quarter. I received a bachelor’s degree in mathematics with
-                a minor in statistics from Cleveland State University where I
-                got a 4.0 GPA and was a Valedictorian Candidate, an Honors
-                Program scholar, and an Outstanding Mathematics Major, among
-                others. I also received an associate degree in financial
-                services from Lorain County Community College where I had a 3.9
-                GPA.
+                I have over 5 years of experience creating software that solve
+                business problems as an analyst in the investment industry,
+                where I spearheaded 8 data analytics and automation projects
+                that saves over 650 hours per quarter. I received a bachelor’s
+                degree in mathematics with a minor in statistics where I got a
+                4.0 GPA and was a Valedictorian Candidate, an Honors Program
+                scholar, and an Outstanding Mathematics Major, among others. I
+                also received an associate degree in financial services where I
+                had a 3.9 GPA.
               </p>
               <p className="lead text-muted">
                 I strive for absolute excellence in my craft. Because of this, I
